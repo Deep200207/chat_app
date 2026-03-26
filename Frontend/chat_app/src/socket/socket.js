46a40c3b100df0ceb365e0fd708aa74a511251f5
Deepak <royannu777@gmail.com>
2026-03-27 01:56:30 +0530

    fix api

--- Frontend/chat_app/src/socket/socket.js
+++ Frontend/chat_app/src/socket/socket.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 // const navigate= useNavigate()
 const user = JSON.parse(localStorage.getItem("chat_user"))
 console.log(user?.email)
-const socket= io("http://localhost:3000",{
+const socket= io("https://chat-app-backend-x8nu.onrender.com",{
     query:{userid: user?.email}
 })
 export default socket;
