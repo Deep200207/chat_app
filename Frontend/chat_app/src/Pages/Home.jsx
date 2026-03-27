1e3da8a7719339bc166be385e97a1f7c24bf6eb8
Deepak <royannu777@gmail.com>
2026-03-27 08:20:35 +0530

    Update Ui

--- Frontend/chat_app/src/Pages/Home.jsx
+++ Frontend/chat_app/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context/UserContext'
-import { MessageCircle, UserCircle2, SendHorizonal, LogOut 
-  
+import {
+  MessageCircle, UserCircle2, SendHorizonal, LogOut
+
 } from 'lucide-react'
 import { io } from 'socket.io-client'
 import socket from '../socket/socket'
@@ -115,6 +116,29 @@ export default function () {
             )
           })}
         </div>
+        {<div className=' bg-slate-100 h-130 overflow-scroll hidden md:flex w-[40%] '>
+          <div className='w-full'>
+            <h1 className=' text-xl bg-white p-2 text-center
+          z-10  fixed md:w-[40%] w-full'>Your Friends</h1>
+            <div className='mt-12 '></div>
+            {friends.map((item, index) => {
+              return (
+                <div key={index} className='w-full bg-gray-300 p-2 mt-1 mb-1  flex'
+                  onClick={() => {
+                    setSelected(item)
+                    setToggle(true)
+                  }}>
+                  <UserCircle2 color='black' className='m-1' size={40}></UserCircle2>
+                  <div>
+                    <h1 className='md:text-lg p-2'>{item.username}</h1>
+                    {/* <h1 className='text-xs '>{item.email}</h1> */}
+                  </div>
+                  {/* <h1 className='text-xs '>{item.email}</h1> */}
+                </div>
+              )
+            })}
+          </div>
+        </div>}
         {!toggle && <div className=' bg-slate-100 h-130 overflow-scroll md:hidden w-full '>
           <h1 className=' text-xl bg-white p-2 text-center
           z-10  fixed md:w-[40%] w-full'>Your Friends</h1>
@@ -136,33 +160,35 @@ export default function () {
             )
           })}
         </div>}
-        {selected ? <div className='w-[60%] hidden'>
-          <div className='w-full h-15 mb-1 mt-1 rounded p-2 bg-emerald-700
+        {selected ? <div className='w-[60%] hidden md:flex'>
+          <div className='w-full'>
+            <div className='w-full h-15 mb-1 mt-1 rounded p-2 bg-emerald-700
           text-white flex'>
-            <UserCircle2 size={40} className='m-1'></UserCircle2>
-            <h1 className='ml-2 m-2 text-xl'>{selected.username}</h1>
-          </div>
-          <div className='bg-slate-200 h-102 overflow-auto'>
-            {
-              message.map((item, index) => {
-                return (
-                  <div key={index} className={`${item.senderId == `${user.email}` ? 'float-right' : 'float-left'} w-full m-1
+              <UserCircle2 size={40} className='m-1'></UserCircle2>
+              <h1 className='ml-2 m-2 text-xl'>{selected.username}</h1>
+            </div>
+            <div className='bg-slate-200 h-102 overflow-auto'>
+              {
+                message.map((item, index) => {
+                  return (
+                    <div key={index} className={`${item.senderId == `${user.email}` ? 'float-right' : 'float-left'} w-full m-1
                     p-1 `}>
-                    <h1 className={`${item.senderId == `${user.email}` ? 'float-right bg-green-500 text-white font-semibold'
-                      : 'float-left bg-white border-white'}
+                      <h1 className={`${item.senderId == `${user.email}` ? 'float-right bg-green-500 text-white font-semibold'
+                        : 'float-left bg-white border-white'}
                       border-1 p-2  max-w-40 rounded  max-h-30 break-words`}
-                    >{item.message}</h1>
-                  </div>
-                )
-              })
-            }
-          </div>
-          <div className='p-1 flex'>
-            <input type="text" className='w-full bg-green-900 h-10 rounded p-1 
+                      >{item.message}</h1>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className='p-1 flex'>
+              <input type="text" className='w-full bg-green-900 h-10 rounded p-1 
             text-white font-semibold outline-0 tracking-wider'
-              value={input} onChange={(e) => setInput(e.target.value)}
-              placeholder='Type Message' />
-            <button onClick={() => handleMessage()}><SendHorizonal size={35} className='m-1'></SendHorizonal></button>
+                value={input} onChange={(e) => setInput(e.target.value)}
+                placeholder='Type Message' />
+              <button onClick={() => handleMessage()}><SendHorizonal size={35} className='m-1'></SendHorizonal></button>
+            </div>
           </div>
         </div> :
           <div className='md:flex justify-center h-120 items-center w-[60%] hidden'>
@@ -171,8 +197,8 @@ export default function () {
               <h1 className='text-center text-2xl tracking-widest'>To Chat</h1>
             </div>
           </div>}
-          {/* Mobile View */}
-        {toggle ? <div className='w-full md:hidden'>
+        {/* Mobile View */}
+        {toggle && <div className='w-full md:hidden'>
           <div className='w-full h-10 mb-1 mt-1 p-2 bg-emerald-700
           text-white flex'>
             <UserCircle2 size={30} className=''></UserCircle2>
@@ -203,13 +229,7 @@ export default function () {
               placeholder='Type Message' />
             <button onClick={() => handleMessage()}><SendHorizonal size={35} className='m-1'></SendHorizonal></button>
           </div>
-        </div> :
-          <div className='md:flex justify-center h-120 items-center w-[60%] hidden'>
-            <div>
-              <h1 className='text-center text-3xl tracking-wider'>Select on Friend </h1>
-              <h1 className='text-center text-2xl tracking-widest'>To Chat</h1>
-            </div>
-          </div>}
+        </div>}
       </main>
     </div>
   )
